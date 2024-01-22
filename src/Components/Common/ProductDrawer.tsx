d9fa4f09c6b161897ca188eefd5df18eead2360f
sufiyan <132542547+sufiyan413@users.noreply.github.com>
2024-01-22 22:55:46 +0530

    modified component and separate css styles

--- src/Components/Common/ProductDrawer.tsx
+++ src/Components/Common/ProductDrawer.tsx
@@ -1,123 +1,19 @@
-import React, { useState } from 'react';
+// ProductDrawer.tsx
+import React, { ChangeEvent, useState } from 'react';
 import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Checkbox from '@mui/material/Checkbox';
-import YouMayLikeCard from './YouMayLikeCard'; 
-import Input from '@mui/material/Input'; 
+import YouMayLikeCard from './YouMayLikeCard';
+import CustomInput from './CustomInput';
+import * as styles from '../Styles/ProductDrawerStyles';
 
 interface ProductDrawerProps {
   open: boolean;
   onClose: () => void;
   product: any;
 }
-
-const drawerStyles = {
-  width: '444px',
-  padding: '30px 42px 42px 42px',
-  backgroundColor: '#F8FAFD',
-} as React.CSSProperties;
-
-const headerStyles = {
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  marginBottom: '20px',
-} as React.CSSProperties;
-
-const contentStyles = {
-  display: 'flex',
-  flexDirection: 'column',
-} as React.CSSProperties;
-
-const h6Styles = {
-  fontWeight: 700,
-  fontSize: '23px',
-  lineHeight: '28.04px',
-} as React.CSSProperties;
-
-const productInfoStyles = {
-  display: 'flex',
-  flexDirection: 'column',
-  marginBottom: '10px',
-  alignItems: 'flex-start',
-} as React.CSSProperties;
-
-const titleStyles = {
-  marginTop: '20px',
-  fontWeight: 700,
-  fontSize: '18px',
-  lineHeight: '21.94px',
-} as React.CSSProperties;
-
-const descriptionStyle = {
-  fontWeight: 400,
-  fontSize: '14px',
-  lineHeight: '20px',
-} as React.CSSProperties;
-
-const imageStyles = {
-  width: '100%',
-  marginTop: '20px',
-  marginBottom: '10px',
-  borderRadius: '7px',
-  maxHeight: '270px',
-} as React.CSSProperties;
-
-const priceStyles = {
-  fontWeight: 700,
-  fontSize: '18px',
-  lineHeight: '21.94px',
-} as React.CSSProperties;
-
-const titlePriceContainerStyles = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  width: '100%',
-} as React.CSSProperties;
-
-const extrasContainerStyles = {
-  marginTop: '20px',
-} as React.CSSProperties;
-
-const checkboxContainerStyles = {
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  marginBottom: '10px',
-} as React.CSSProperties;
-
-const checkboxStyles = {
-  display: 'flex',
-  alignItems: 'center',
-} as React.CSSProperties;
-
-const checkboxNameStyles = {
-  marginLeft: '10px',
-} as React.CSSProperties;
-
-const youMayAlsoLikeContainerStyles = {
-  marginTop: '20px',
-} as React.CSSProperties;
-
-const specificPreferencesContainerStyles = {
-  marginTop: '20px',
-} as React.CSSProperties;
-
-const inputStyles = {
-  borderRadius: '4px',
-  backgroundColor: '#FFFFFF',
-  width: '100%',
-  padding: '8px',
-  boxSizing: 'border-box',
-  marginTop: '8px',
-  fontWeight: 400,
-  fontSize: '14px',
-  lineHeight: '17.07px',
-} as React.CSSProperties;
 
 const ProductDrawer: React.FC<ProductDrawerProps> = ({ open, onClose, product }) => {
   if (!product) return null;
@@ -131,14 +27,14 @@ const ProductDrawer: React.FC<ProductDrawerProps> = ({ open, onClose, product })
         'https://images.pexels.com/photos/50593/coca-cola-cold-drink-soft-drink-coke-50593.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
-      id: 16,
+      id: 17,
       title: 'Special Drink',
       price: 4.22,
       image:
         'https://images.pexels.com/photos/50593/coca-cola-cold-drink-soft-drink-coke-50593.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     },
     {
-      id: 16,
+      id: 18,
       title: 'Coca Cola',
       price: 4.22,
       image:
@@ -146,73 +42,95 @@ const ProductDrawer: React.FC<ProductDrawerProps> = ({ open, onClose, product })
     },
   ];
 
+  const [description, setDescription] = useState<string>('');
+  const [counter, setCounter] = useState<number>(1);
+  const [isIncreaseClicked, setIsIncreaseClicked] = useState<boolean>(false);
+  const [isDecreaseClicked, setIsDecreaseClicked] = useState<boolean>(false);
+
+  const handleIncrease = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+    setIsIncreaseClicked(true);
+    setIsDecreaseClicked(false);
+  };
+
+  const handleDecrease = () => {
+    if (counter > 1) {
+      setCounter((prevCounter) => prevCounter - 1);
+      setIsDecreaseClicked(true);
+      setIsIncreaseClicked(false);
+    }
+  };
+
+  const handleAddToCart = () => {
+    console.log(`Added ${counter} ${product.title}(s) to the cart`);
+  };
 
   return (
     <Drawer anchor="right" open={open} onClose={onClose}>
-      <div style={{ ...drawerStyles, width: '100%', maxWidth: '500px' }}>
-        <div style={headerStyles}>
-          <Typography variant="h6" sx={h6Styles}>
+      <div style={{ ...styles.drawerStyles, width: '100%', maxWidth: '500px' }}>
+        <div style={styles.headerStyles}>
+          <Typography variant="h6" sx={styles.h6Styles}>
             Product Details
           </Typography>
-          <IconButton onClick={onClose} sx={h6Styles}>
+          <IconButton onClick={onClose} sx={styles.h6Styles}>
             <CloseIcon />
           </IconButton>
         </div>
-        <div style={contentStyles}>
-          <div style={productInfoStyles}>
-            <div style={titlePriceContainerStyles}>
-              <Typography variant="h6" sx={titleStyles}>
+        <div style={styles.contentStyles}>
+          <div style={styles.productInfoStyles}>
+            <div style={styles.titlePriceContainerStyles}>
+              <Typography variant="h6" sx={styles.titleStyles}>
                 {product.title}
               </Typography>
-              <Typography variant="h6" sx={priceStyles}>
+              <Typography variant="h6" sx={styles.priceStyles}>
                 € {product.price}
               </Typography>
             </div>
-            <img src={product.image} alt={product.title} style={imageStyles} />
-            <Typography variant="body2" sx={descriptionStyle}>
+            <img src={product.image} alt={product.title} style={styles.imageStyles} />
+            <Typography variant="body2" sx={styles.descriptionStyle}>
               {product.description}
             </Typography>
           </div>
-          <div style={extrasContainerStyles}>
-            <Typography variant="h6" sx={titleStyles}>
+          <div style={styles.extrasContainerStyles}>
+            <Typography variant="h6" sx={styles.titleStyles}>
               Extras
             </Typography>
-            <div style={checkboxContainerStyles}>
-              <div style={checkboxStyles}>
+            <div style={styles.checkboxContainerStyles}>
+              <div style={styles.checkboxStyles}>
                 <Checkbox />
-                <Typography variant="body1" sx={checkboxNameStyles}>
+                <Typography variant="body1" sx={styles.checkboxNameStyles}>
                   Bacon
                 </Typography>
               </div>
-              <Typography variant="h6" sx={priceStyles}>
+              <Typography variant="h6" sx={styles.priceStyles}>
                 € 2.50
               </Typography>
             </div>
-            <div style={checkboxContainerStyles}>
-              <div style={checkboxStyles}>
+            <div style={styles.checkboxContainerStyles}>
+              <div style={styles.checkboxStyles}>
                 <Checkbox />
-                <Typography variant="body1" sx={checkboxNameStyles}>
+                <Typography variant="body1" sx={styles.checkboxNameStyles}>
                   Egg
                 </Typography>
               </div>
-              <Typography variant="h6" sx={priceStyles}>
+              <Typography variant="h6" sx={styles.priceStyles}>
                 € 2.50
               </Typography>
             </div>
-            <div style={checkboxContainerStyles}>
-              <div style={checkboxStyles}>
+            <div style={styles.checkboxContainerStyles}>
+              <div style={styles.checkboxStyles}>
                 <Checkbox />
-                <Typography variant="body1" sx={checkboxNameStyles}>
+                <Typography variant="body1" sx={styles.checkboxNameStyles}>
                   Wedges
                 </Typography>
               </div>
-              <Typography variant="h6" sx={priceStyles}>
+              <Typography variant="h6" sx={styles.priceStyles}>
                 € 2.50
               </Typography>
             </div>
           </div>
-          <div style={youMayAlsoLikeContainerStyles}>
-            <Typography variant="h6" sx={titleStyles}>
+          <div style={styles.youMayAlsoLikeContainerStyles}>
+            <Typography variant="h6" sx={styles.titleStyles}>
               You may also like
             </Typography>
             <div style={{ display: 'flex', marginTop: '20px' }}>
@@ -221,11 +139,37 @@ const ProductDrawer: React.FC<ProductDrawerProps> = ({ open, onClose, product })
               ))}
             </div>
           </div>
-          <div style={specificPreferencesContainerStyles}>
-            <Typography variant="h6" sx={titleStyles}>
+          <div style={styles.specificPreferencesContainerStyles}>
+            <Typography variant="h6" sx={styles.titleStyles}>
               Specific Preferences
             </Typography>
-            <Input placeholder="Add description..." style={inputStyles} />
+            <CustomInput
+              label="Add description..."
+              value={description}
+              onChange={(value) => setDescription(value)}
+              width="100%"
+              height="40px"
+              padding="8px"
+              margin="10px"
+              borderStyle="none"
+              borderRadius="8px"
+              backgroundColor="white"
+              color="gray"
+            />
+          </div>
+          <div style={styles.rowContainer}>
+            <div style={styles.counterSectionStyles}>
+              <div style={styles.counterButtonStyles(isDecreaseClicked)} onClick={handleDecrease}>
+                -
+              </div>
+              <div style={styles.counterNumberStyles(isIncreaseClicked)}>{counter}</div>
+              <div style={styles.counterButtonStyles(isIncreaseClicked)} onClick={handleIncrease}>
+                +
+              </div>
+            </div>
+            <div style={styles.addToCartButtonStyles} onClick={handleAddToCart}>
+              Add to Cart
+            </div>
           </div>
         </div>
       </div>
