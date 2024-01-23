2fb63b540df64e4a235b2808cba5058cbc4b8445
sufiyan <132542547+sufiyan413@users.noreply.github.com>
2024-01-23 20:51:17 +0530

    added Api for Menu list

--- src/Components/SubHeader/Subheader.tsx
+++ src/Components/SubHeader/Subheader.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import './Subheader.css';
 import { NavLink } from 'react-router-dom';
 import IconButton from '@mui/material/IconButton';
@@ -8,14 +9,35 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import { useMediaQuery, useTheme } from '@mui/material';
-import CustomInput from '../Common/CustomInput'
+import CustomInput from '../Common/CustomInput';
+import { fetchMenuCategories, MenuCategory } from '../ApiServices/Api';
 
-interface SubheaderProps {}
+
+
+interface SubheaderProps { }
 
 const Subheader: React.FC<SubheaderProps> = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
   const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const [menuCategories, setMenuCategories] = useState<MenuCategory[]>([]);
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+        const response = await fetchMenuCategories();
+        const { data } = response;
+        setMenuCategories(data.menuCategories);
+        console.log("Data received:", data);
+      } catch (error) {
+        console.error('Error fetching menu categories:', error);
+      }
+    };
+
+    fetchData();
+
+  }, []);
 
   const toggleDrawer = (open: boolean) => {
     setDrawerOpen(open);
@@ -28,86 +50,36 @@ const Subheader: React.FC<SubheaderProps> = () => {
           <IconButton color="inherit" onClick={() => toggleDrawer(true)}>
             <MenuIcon />
           </IconButton>
-          
-          <CustomInput 
-                       label="Search" 
-                       value="" onChange={() => {}} 
-                       width="100%" height="50px" 
-                       padding="0 10px" 
+
+          <CustomInput
+            label="Search"
+            value=""
+            onChange={() => { }}
+            width="100%"
+            height="50px"
+            padding="0 10px"
           />
+
         </div>
       ) : (
+
         <div className="subheader">
-          <NavLink to="/appetizers" className="subheader-link">
-            Appetizers
-          </NavLink>
-          <NavLink to="/burgers" className="subheader-link">
-            Burgers
-          </NavLink>
-          <NavLink to="/desserts" className="subheader-link">
-            Desserts
-          </NavLink>
-          <NavLink to="/salads" className="subheader-link">
-            Salads
-          </NavLink>
-          <NavLink to="/pizza" className="subheader-link">
-            Pizza
-          </NavLink>
-          <NavLink to="/barbeque" className="subheader-link">
-            Barbeque
-          </NavLink>
-          <NavLink to="/soups" className="subheader-link">
-            Soups
-          </NavLink>
-          <NavLink to="/moms-home" className="subheader-link">
-            Mom's Home
-          </NavLink>
-          <NavLink to="/breakfast" className="subheader-link">
-            Breakfast
-          </NavLink>
-          <NavLink to="/drinks" className="subheader-link">
-            Drinks
-          </NavLink>
-          <NavLink to="/sauces" className="subheader-link">
-            Sauces
-          </NavLink>
+          {menuCategories.map(category => (
+            <NavLink key={category.id} to={`${category.id}`} className="subheader-link">
+              {category.title}
+            </NavLink>
+          ))}
+
         </div>
       )}
+
       <Drawer anchor="left" open={isDrawerOpen} onClose={() => toggleDrawer(false)}>
         <List>
-          <ListItemButton component={NavLink} to="/appetizers">
-            <ListItemText primary="Appetizers" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/burgers">
-            <ListItemText primary="Burgers" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/desserts">
-            <ListItemText primary="Desserts" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/salads">
-            <ListItemText primary="Salads" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/pizza">
-            <ListItemText primary="Pizza" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/barbeque">
-            <ListItemText primary="Barbeque" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/soups">
-            <ListItemText primary="Soups" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/moms-home">
-            <ListItemText primary=" Mom's Home" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/breakfast">
-            <ListItemText primary="Breakfast" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/drinks">
-            <ListItemText primary="Drinks" />
-          </ListItemButton>
-          <ListItemButton component={NavLink} to="/sauces">
-            <ListItemText primary="Sauces" />
-          </ListItemButton>
+          {menuCategories.map(category => (
+            <ListItemButton key={category.id} component={NavLink} to={`${category.id}`}>
+              <ListItemText primary={category.title} />
+            </ListItemButton>
+          ))}
         </List>
       </Drawer>
     </>
