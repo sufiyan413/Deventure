707561a959aff2c308f221667a6f26f4aeafeea8
sufiyan <132542547+sufiyan413@users.noreply.github.com>
2024-01-23 20:49:31 +0530

    implement Api and render card data throgh api

--- src/Pages/Burgers.tsx
+++ src/Pages/Burgers.tsx
@@ -1,140 +1,92 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ProductCard from '../Components/Common/ProductCard';
 import ProductDrawer from '../Components/Common/ProductDrawer';
 import Grid from '@mui/material/Grid';
 import { Box } from '@mui/material';
+import { ProductMenuList } from '../Components/ApiServices/Api';
+
+interface MenuItem {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+}
 
 const Burgers: React.FC = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState<any>(null);
+  const [selectedProduct, setSelectedProduct] = useState<MenuItem | null>(null);
+  const [productMenuList, setProductMenuList] = useState<MenuItem[]>([]);
 
-  const burgerData = [
-    {
-      id: 1,
-      title: 'Jack’s Burger',
-      description: 'Sesame bun, beef, bacon, mozzarella, homemade, 600g',
-      price: 8.12,
-      image: 'https://images.pexels.com/photos/1251198/pexels-photo-1251198.jpeg',
-    },
-    {
-      id: 2,
-      title: 'Old Town Burger',
-      description: 'Sesame bun, beef, bacon, cheddar, tomatoes, onion, 600g',
-      price: 8.12,
-      image: 'https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 3,
-      title: 'Veggie Fresh Burger',
-      description: 'Sesame Bun, Baby Spinach, Pumpkin, Eggplant, 600g',
-      price: 8.12,
-      image: 'https://images.pexels.com/photos/17971406/pexels-photo-17971406/free-photo-of-veg-burger.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 4,
-      title: 'Cheese Meltdown Burger',
-      description: 'Sesame Bun, Beef, Bacon, Parmesan, Cheddar, 600g',
-      price: 8.12,
-      image: 'https://images.pexels.com/photos/3915906/pexels-photo-3915906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 5,
-      title: 'Mr. Chili Pepper ',
-      description: 'Sesame bun, beef, bacon, mozzarella, homemade, 600g',
-      price: 8.12,
-      image: 'https://images.pexels.com/photos/8879375/pexels-photo-8879375.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 6,
-      title: 'Laid Back Chicken Burger',
-      description: 'Sesame bun, beef, bacon, cheddar, tomatoes, onion, 600g',
-      price: 8.12,
-      image: 'https://images.pexels.com/photos/6896379/pexels-photo-6896379.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 7,
-      title: 'Run Away Gorgonzola ',
-      description: 'Sesame Bun, Baby Spinach, Pumpkin, 600g',
-      price: 8.12,
-      image: 'https://images.pexels.com/photos/12034622/pexels-photo-12034622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
+  useEffect(() => {
+    getMenuList();
+    // console.log("testing data");
+  }, []);
+
+  // const getMenuList = async () => {
+  //   const payload = {
+  //     Page: 0,
+  //     PageSize: 50,
+  //     RestaurantId: 37,
+  //     Term: "",
+  //   };
+  //   try {
+  //     const res = await axios.post(ProductMenuList, payload);
+  //     const productList =
+  //       res.data && res.data.data && res.data.data.data.length > 0
+  //         ? res.data.data.data
+  //         : [];
+
+  //     setProductMenuList(productList);
+  //     console.log(res);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  const CustomImage = 'https://images.pexels.com/photos/1600727/pexels-photo-1600727.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 
 
-  ];
-
-  const dessertData = [
-    {
-      id: 8,
-      title: 'Home Creams',
-      description: '200g',
-      price: 6.99,
-      image: 'https://images.pexels.com/photos/2144112/pexels-photo-2144112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 9,
-      title: 'Gray Pudding with Fruit Jam',
-      description: '150g',
-      price: 6.99,
-      image: 'https://images.pexels.com/photos/2693447/pexels-photo-2693447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 10,
-      title: 'Caramelized apple pie and ice cream',
-      description: '200g',
-      price: 6.99,
-      image: 'https://images.pexels.com/photos/221083/pexels-photo-221083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-    {
-      id: 11,
-      title: 'Chocolate Lava Cake & Ice Cream',
-      description: '150 g',
-      price: 6.99,
-      image: 'https://images.pexels.com/photos/703115/pexels-photo-703115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    },
-  ];
-
-  const saladData = [
-    {
-      id: 12,
-      title: 'Classic Caesar Salad',
-      description: 'Romaine lettuce, croutons, parmesan cheese, Caesar dressing',
-      price: 7.99,
-      image: 'https://images.pexels.com/photos/1211887/pexels-photo-1211887.jpeg?auto=compress&cs=tinysrgb&w=600',
-    },
-    {
-      id: 13,
-      title: 'Greek Salad',
-      description: 'Mixed greens, cherry tomatoes, cucumber, feta cheese, olives,',
-      price: 8.99,
-      image: 'https://images.pexels.com/photos/13467083/pexels-photo-13467083.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-    },
-    {
-      id: 14,
-      title: 'Greek Salad',
-      description: 'Mixed greens, cherry tomatoes, cucumber, feta cheese, olives,',
-      price: 8.99,
-      image: 'https://images.pexels.com/photos/764925/pexels-photo-764925.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
-    },
-    {
-      id: 15,
-      title: 'Greek Salad',
-      description: 'Mixed greens, cherry tomatoes, cucumber, feta cheese, olives,',
-      price: 8.99,
-      image: 'https://images.pexels.com/photos/1833334/pexels-photo-1833334.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+  const getMenuList = async () => {
+    const payload = {
+      Page: 0,
+      PageSize: 50,
+      RestaurantId: 37,
+      Term: "",
+    };
+    try {
+      const res = await axios.post(ProductMenuList, payload);
+      const productList =
+        res.data && res.data.data && res.data.data.data.length > 0
+          ? res.data.data.data.map((product: MenuItem) => ({
+              ...product,
+              image: CustomImage,
+            }))
+          : [];
+  
+      setProductMenuList(productList);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
     }
-  ];
+  };
 
-  const handleCardClick = (product: any) => {
+  
+  const handleCardClick = (product: MenuItem) => {
     setSelectedProduct(product);
     setDrawerOpen(true);
   };
+
+  // const dessertData: MenuItem[] = productMenuList;
+  // const saladData: MenuItem[] = productMenuList;
 
   return (
     <Box sx={{ pt: '15px', pl: '60px', pr: '30px', backgroundColor: '#eaeff8' }}>
       <Box sx={{ pt: 3, pb: 2 }}>
         <h1 style={{ fontSize: '23px', lineHeight: '28px', paddingBottom: '20px' }}>Burgers</h1>
         <Grid container spacing={2}>
-          {burgerData.map((burger) => (
+          {productMenuList.map((burger: MenuItem) => (
             <Grid key={burger.id} item xs={12} sm={6} md={4} lg={3}>
               <ProductCard
                 image={burger.image}
@@ -148,10 +100,10 @@ const Burgers: React.FC = () => {
         </Grid>
       </Box>
 
-      <Box sx={{ pt: 6, pb: 2 }}>
+      {/* <Box sx={{ pt: 6, pb: 2 }}>
         <h1 style={{ fontSize: '23px', lineHeight: '28px', paddingBottom: '20px' }}>Desserts</h1>
         <Grid container spacing={2}>
-          {dessertData.map((dessert) => (
+          {dessertData.map((dessert: MenuItem) => (
             <Grid key={dessert.id} item xs={12} sm={6} md={4} lg={3}>
               <ProductCard
                 image={dessert.image}
@@ -163,12 +115,12 @@ const Burgers: React.FC = () => {
             </Grid>
           ))}
         </Grid>
-      </Box>
+      </Box> */}
 
-      <Box sx={{ pt: 6, pb: 2 }}>
+      {/* <Box sx={{ pt: 6, pb: 2 }}>
         <h1 style={{ fontSize: '23px', lineHeight: '28px', paddingBottom: '20px' }}>Salads</h1>
         <Grid container spacing={2}>
-          {saladData.map((salad) => (
+          {saladData.map((salad: MenuItem) => (
             <Grid key={salad.id} item xs={12} sm={6} md={4} lg={3}>
               <ProductCard
                 image={salad.image}
@@ -180,7 +132,7 @@ const Burgers: React.FC = () => {
             </Grid>
           ))}
         </Grid>
-      </Box>
+      </Box> */}
 
       <ProductDrawer
         open={drawerOpen}
@@ -192,6 +144,3 @@ const Burgers: React.FC = () => {
 };
 
 export default Burgers;
-
-
-
