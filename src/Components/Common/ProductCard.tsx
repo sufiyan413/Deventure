61217fa20c25164ca07f9dcbd7f868c8e0d08aa0
sufiyan <132542547+sufiyan413@users.noreply.github.com>
2024-01-24 18:53:29 +0530

    modified custom css

--- src/Components/Common/ProductCard.tsx
+++ src/Components/Common/ProductCard.tsx
@@ -6,6 +6,13 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
 import Box from '@mui/material/Box';
+import {
+            cardStyle,
+            cardMediaStyle,
+            descriptionStyle,
+            priceBoxStyle,
+            priceStyle,
+} from '../Styles/ProductCard'; 
 
 interface ProductCardProps {
   image: string;
@@ -17,18 +24,18 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ image, title, description, price, onCardClick }) => {
   return (
-    <Card style={{ width: '94%', borderRadius: '10px' }} onClick={onCardClick}>
-      <CardMedia component="img" height="250" style={{ padding: '20px 15px 0px 15px', borderRadius: '25px' }} image={image} alt={title} />
+    <Card style={cardStyle} onClick={onCardClick}>
+      <CardMedia component="img" height="250" style={cardMediaStyle} image={image} alt={title} />
       <CardContent>
         <Typography variant="h6" component="div" style={{ fontWeight: "700" }}>
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary" style={{ fontSize: '1.1rem', marginTop: '5px', width: '90%' }}>
+        <Typography variant="body2" color="text.secondary" style={descriptionStyle}>
           {description}
         </Typography>
 
-        <Box display="flex" justifyContent="space-between" alignItems="center" marginTop="20px">
-          <Typography variant="h6" style={{ fontWeight: '600' }}>€ {price}</Typography>
+        <Box style={priceBoxStyle}>
+          <Typography variant="h6" style={priceStyle}>€ {price}</Typography>
           <IconButton
             aria-label="add to cart"
             style={{ borderRadius: '50%', border: '1px solid' }}
