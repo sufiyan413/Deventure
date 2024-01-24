d490e9f1eff9734db1582374a4dade9a4d4aa904
sufiyan <132542547+sufiyan413@users.noreply.github.com>
2024-01-24 18:54:57 +0530

    created new component for separate custom css

---
+++ src/Components/Styles/ProductCard.ts
@@ -0,0 +1,33 @@
+
+export const cardStyle = {
+    width: '94%',
+    borderRadius: '10px',
+    overflow: 'hidden',
+  };
+  
+  export const cardMediaStyle = {
+    padding: '20px 15px 0px 15px',
+    borderRadius: '25px',
+    objectFit: 'cover',
+  };
+  
+  export const descriptionStyle = {
+    fontSize: '1.1rem',
+    marginTop: '5px',
+    width: '100%',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    minHeight: '100px',
+  };
+  
+  export const priceBoxStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: '20px',
+  };
+  
+  export const priceStyle = {
+    fontWeight: '600',
+  };
+  
