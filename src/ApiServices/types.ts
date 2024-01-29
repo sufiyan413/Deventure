dcaf7ad992f6b3789b1654fef2fc5cdd68bc6888
sufiyan <132542547+sufiyan413@users.noreply.github.com>
2024-01-29 18:10:15 +0530

    separate api section for handle data

---
+++ src/ApiServices/types.ts
@@ -0,0 +1,18 @@
+// export interface MenuItem {
+//     id: number;
+//     title: string;
+//     imageUrl: string;
+//     description: string;
+//     rating: number;
+//     price: number;
+//     allergens: string[];
+//   }
+  
+//   export interface MenuCategory {
+//     id: number;
+//     title: string;
+//     imageUrl: string;
+//   }
+
+  
+  
